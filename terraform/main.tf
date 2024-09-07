babd2f5895cdd4f371d30613e19c2118ca4265ba
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2024-09-07 17:34:30 +0900

    Deploy hello-world app with terraform

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+resource "aws_apprunner_service" "hello_world" {
+  service_name = "hello-world"
+
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+
+      image_identifier      = "943566097936.dkr.ecr.ap-northeast-1.amazonaws.com/hello-world:v0.1.0"
+      image_repository_type = "ECR"
+    }
+
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::943566097936:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    auto_deployments_enabled = false
+  }
+
+  instance_configuration {
+    cpu    = "256"
+    memory = "512"
+  }
+}
